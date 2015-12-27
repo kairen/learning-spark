670201853ad855257cbc8865782f0b4a4657a432
kairen <kyle.b@inwinstack.com>
2015-12-28 01:47:13 +0800

    Add mesos-slave install ...

--- scripts/mesos-deploy/config.sh
+++ scripts/mesos-deploy/config.sh
@@ -10,7 +10,7 @@
 function master-config {
 	ssh $1 echo 1 | sudo tee /etc/zookeeper/conf/myid
 	ssh $1 echo server.1=$(ip route get 8.8.8.8 | awk '{print $NF; exit}'):2888:3888 | sudo tee -a /etc/zookeeper/conf/zoo.cfg
-	ssh $1 sudo service zookeeper restart
+	ssh $1 sudo service zookeeper restart &>/dev/null
 
 	# Configure mesos-master
 	ssh $1 echo zk://$(ip route get 8.8.8.8 | awk '{print $NF; exit}'):2181/mesos | sudo tee /etc/mesos/zk
@@ -25,10 +25,15 @@ function master-config {
 	ssh $1 echo $(ip route get 8.8.8.8 | awk '{print $NF; exit}') | sudo tee /etc/marathon/conf/hostname
 	ssh $1 echo zk://$(ip route get 8.8.8.8 | awk '{print $NF; exit}'):2181/mesos | sudo tee /etc/marathon/conf/master
 	ssh $1 echo zk://$(ip route get 8.8.8.8 | awk '{print $NF; exit}'):2181/marathon | sudo tee /etc/marathon/conf/zk
-	ssh $1 sudo service mesos-slave stop
-	ssh $1 sudo sh -c "echo manual > /etc/init/mesos-slave.override"
-	ssh $1 sudo service mesos-master restart
-	ssh $1 sudo service marathon restart
+	
+	ssh $1 sudo service mesos-slave stop &>/dev/null
+	ssh $1 echo manual | sudo tee /etc/init/mesos-slave.override
+
+	ssh $1 sudo service mesos-master restart &>/dev/null
+	ssh $1 sudo service marathon restart &>/dev/null
 }
 
+function slave-config {
+	
+}
 
